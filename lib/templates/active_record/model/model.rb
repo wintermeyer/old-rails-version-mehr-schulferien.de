a13e480006d17d18efa4906796947685647251c0
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2014-12-17 15:42:42 +0100

    Add touch: true for belongs_to associations.

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -14,7 +14,7 @@ class <%= class_name %> < <%= parent_class_name.classify %>
   # Associations
   #
 <% attributes.select(&:reference?).each do |attribute| -%>
-  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
+  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>, touch: true
 <% end -%>
 <% if attributes.any?(&:password_digest?) -%>
   has_secure_password
